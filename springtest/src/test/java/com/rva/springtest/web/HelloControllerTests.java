df4f8c416af1ab891560f89c10e838d762638131
Ramón Viñas Avalos <rvinasav@isban.es>
2014-09-17 23:47:35 +0200

    V1: Desarrollando y Configurando la Vista y el Controlador

---
+++ springtest/src/test/java/com/rva/springtest/web/HelloControllerTests.java
@@ -0,0 +1,18 @@
+package com.rva.springtest.web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+public class HelloControllerTests {
+	@Test
+    public void testHandleRequestView() throws Exception{		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("WEB-INF/views/hello.jsp", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        String nowValue = (String) modelAndView.getModel().get("now");
+        assertNotNull(nowValue);
+    }
+}
